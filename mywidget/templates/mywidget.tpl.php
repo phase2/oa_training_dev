6927be12941dc5466e9b812f27ecc75f8a71be22
Mike Potter <mpotter@phase2technology.com>
2014-10-14 17:40:49 -0400

    Add widget example

---
+++ mywidget/templates/mywidget.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Provides the template for the node token widget.
+ *
+ * $body - HTML body
+ */
+?>
+<div class="clearfix" style="<?php print $styles; ?>">
+  <h3><?php print $node_title; ?></h3>
+  <p><?php print $body; ?></p>
+</div>
